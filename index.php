e676c87df45bd44e82b0428e0e073875b45aea1e
Eduard Lipov <revoulce@localhost.localdomain>
2020-10-28 19:30:05 +0300

    Deployment test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/php/sql.php';
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <!-- Haha -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="favicon.ico">
